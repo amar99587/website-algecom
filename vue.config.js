eaaa58b0442c648e84eb3af3dd82349d0434c7e9
amar99587 <72984889+amar99587@users.noreply.github.com>
2025-05-30 12:37:54 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@ const { defineConfig } = require('@vue/cli-service');
 const fs = require('fs');
 
 module.exports = defineConfig({
+  publicPath: '/algecom/', // 👈 VERY IMPORTANT for GitHub Pages
   transpileDependencies: true,
   devServer: {
     https: {
       key: fs.readFileSync('./localhost-key.pem'),
       cert: fs.readFileSync('./localhost.pem'),
     },
-    port: 8080, // or whatever you like
-  }
-})
-  
+    port: 8080,
+  },
+});
