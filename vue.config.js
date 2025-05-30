9366d3fc4d7ee0b2b612d181ca56ba1752bd7bbe
amar99587 <72984889+amar99587@users.noreply.github.com>
2025-05-30 12:35:22 +0100

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 const fs = require('fs');
 
 module.exports = defineConfig({
-  base: '/website-algecom/',
   transpileDependencies: true,
   devServer: {
     https: {
